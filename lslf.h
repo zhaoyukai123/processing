abf7ac6535959e8338bb88bbe47b871769792888
zhaoyukai123 <649471834@qq.com>
2020-01-02 03:56:25 +0800

    Update lslf.h

--- lslf.h
+++ lslf.h
@@ -1,5 +1,5 @@
-#include "fftw3.h"
-void lslf(double y[][1000],double *x)
+#include "stdlib.h"
+void lslf(double *y,double *x)
 {
 	double a,b;
 	double sum_x2,sum_y,sum_x,sum_xy;
@@ -7,14 +7,14 @@ for(int j=0;j<1000;j++){
 	sum_x2 = 0.0;sum_y  = 0.0;sum_x  = 0.0;sum_xy = 0.0;
 	for (int i = 0; i < 3200; ++i) {
 	sum_x2 += x[i]*x[i];
-	sum_y+=y[i][j];
+	sum_y+=y[i+j*3200];
 	sum_x+=x[i];
-	sum_xy += x[i]*y[i][j];
+	sum_xy += x[i]*y[i+j*3200];
 }
 	a = (3200*sum_xy - sum_x*sum_y)/(3200*sum_x2 - sum_x*sum_x);
     b = (sum_x2*sum_y - sum_x*sum_xy)/(3200*sum_x2-sum_x*sum_x);
 	for (int i = 0; i < 3200; ++i) {
-		y[i][j]-=(a*x[i]+b);
+		y[i+j*3200]-=(a*x[i]+b);
 	}
 }
 }
