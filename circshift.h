3fee75ad2e4dad88ef897083ee44567ae4591217
zhaoyukai123 <649471834@qq.com>
2020-01-02 03:54:32 +0800

    Update circshift.h

--- circshift.h
+++ circshift.h
@@ -2,15 +2,15 @@
 #include "stdlib.h"
 #include <math.h>
 
-void circshift(double cor_data[][1000],int *shift,int length){
+void circshift(double *cor_data,int *shift,int length){
 	double temp[6400];
 	for (int i=0;i<length;i++){
 		for(int j=0;j<3200;j++){
-			temp[j+3200]=cor_data[j][i];
-			temp[j]=cor_data[j][i];
+			temp[j+3200]=cor_data[j+i*3200];
+			temp[j]=cor_data[j+i*3200];
 		}
 		for(int j=0;j<3200;j++){
-			cor_data[j][i]=temp[j+shift[i]];
+			cor_data[j+i*3200]=temp[j+shift[i]];
 		}
 	}
 }
