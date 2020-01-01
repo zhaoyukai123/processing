0463c28df8e28bf6c7cbe82a85250d42e9bd9f89
zhaoyukai123 <649471834@qq.com>
2020-01-02 03:55:07 +0800

    Update cor.h

--- cor.h
+++ cor.h
@@ -1,19 +1,11 @@
 #include "stdlib.h"
-void cor(double cor_data[][1000],double in[][10000]){
-	double temp[10000][3200];
-	for (int i=0;i<10000;i++){
-		for (int j=0;j<3200;j++){
-			temp[i][j]=in[j][i];
-	}
-	for(int i=0;i<1000;i++){
+#include "tran.h"
+void cor(double *cor_data,double *in,double *temp1,double *temp2,int n){
+	tran(in,0,0,n,3200,temp1);
+	for(int i=0;i<n/10;i++){
 		for(int j=0;j<3200;j++){
-			temp[i*10][j]=(temp[i*10][j]+temp[i*10+1][j]+temp[i*10+2][j]+temp[i*10+3][j]+temp[i*10+4][j]+temp[i*10+5][j]+temp[i*10+6][j]+temp[i*10+7][j]+temp[i*10+8][j]+temp[i*10+9][j])/10;
+			cor_data[i*3200+j]=(temp1[i*10+j]+temp1[i*10+1+j]+temp1[i*10+2+j]+temp1[i*10+3+j]+temp1[i*10+4+j]+temp1[i*10+5+j]+temp1[i*10+6+j]+temp1[i*10+7+j]+temp1[i*10+8+j]+temp1[i*10+9+j])/10;
 		}
 	}
-	for (int i=0;i<1000;i++){
-		for (int j=0;j<3200;j++){
-			cor_data[j][i]=temp[i][j];
-		}
-	}
-}
+	tran(cor_data,0,0,n/10,3200,temp2);
 }
